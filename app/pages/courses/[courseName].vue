e2f3f06c80126dfe0cfa8e419e7805b224068bde
Amgad000 <almuhamady.amgad001@gmail.com>
2025-10-25 18:02:43 +0300

    same as previous

--- app/pages/courses/[courseName].vue
+++ app/pages/courses/[courseName].vue
@@ -1,45 +1,43 @@
 <template>
-  <UContainer>
-    <h2 class="text-primary-600 text-4xl">
-      this is {{ $route.params.courseName }} course
-    </h2>
-    <div class="grid min-h-screen grid-cols-12 gap-2">
-      <nav class="col-span-4 bg-neutral-100 p-4 dark:bg-neutral-800">
-        <h2 class="mb-4 text-2xl font-bold">Chapters</h2>
-        <ul>
-          <li
-            class="text-xl"
-            v-for="chapter in specificCourse.chapters"
-            :key="chapter.id"
-          >
-            {{ `${chapter.id}: ${chapter.title}` }}
-            <ul>
-              <NuxtLink
-                class="hover:bg-accented my-2.5 block px-4 text-base"
-                v-for="lesson in chapter.lessons"
-                :key="lesson.id"
-                :to="`/courses/${specificCourse.url}/chapters/${chapter.id}/lessons/${lesson.id}`"
-              >
-                {{ lesson.id }}. {{ lesson.title }}
-              </NuxtLink>
-            </ul>
-          </li>
-        </ul>
-      </nav>
+  <h2 class="text-primary-600 py-4 text-center text-4xl capitalize">
+    this is {{ specificCourse.title }} course
+  </h2>
+  <div class="grid min-h-screen grid-cols-12 gap-2">
+    <nav class="col-span-4 bg-neutral-100 p-4 dark:bg-neutral-800">
+      <h2 class="mb-4 text-2xl font-bold">Chapters</h2>
+      <ul>
+        <li
+          class="text-xl"
+          v-for="chapter in specificCourse.chapters"
+          :key="chapter.id"
+        >
+          {{ `${chapter.id}: ${chapter.title}` }}
+          <ul>
+            <NuxtLink
+              class="hover:bg-accented my-2.5 block px-4 text-base"
+              v-for="lesson in chapter.lessons"
+              :key="lesson.id"
+              :to="`/courses/${specificCourse.url}/chapters/${chapter.id}/lessons/${lesson.id}`"
+            >
+              {{ lesson.id }}. {{ lesson.title }}
+            </NuxtLink>
+          </ul>
+        </li>
+      </ul>
+    </nav>
 
-      <main class="col-span-8 bg-neutral-100 p-4 text-lg dark:bg-neutral-800">
-        <!-- only in course overview page using $route.params. -->
-        <div v-if="!$route.params.chapterSlug">
-          <h2 class="mb-4 text-2xl font-bold">
-            an Overview of {{ specificCourse.title }}
-          </h2>
-          <p>{{ specificCourse.description }}</p>
-        </div>
+    <main class="col-span-8 bg-neutral-100 p-4 text-lg dark:bg-neutral-800">
+      <!-- only in course overview page using $route.params. -->
+      <div v-if="!$route.params.chapterSlug">
+        <h2 class="mb-4 text-2xl font-bold">
+          an Overview of {{ specificCourse.title }}
+        </h2>
+        <p>{{ specificCourse.description }}</p>
+      </div>
 
-        <NuxtPage />
-      </main>
-    </div>
-  </UContainer>
+      <NuxtPage />
+    </main>
+  </div>
 </template>
 
 <script setup>
